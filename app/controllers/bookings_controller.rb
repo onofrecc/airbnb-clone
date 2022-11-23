e02b3ac6a77ea8b3b3d1b88b63a0d7c749e7687f
onofre catala castellar <onofrecc@gmail.com>
2022-11-23 12:01:04 +0100

    Implemented booking

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,20 @@
+class BookingsController < ApplicationController
+  def create
+    @flat = Flat.find(params[:flat_id])
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.flat = @flat
+
+    if @booking.save
+      redirect_to flats_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
+end
